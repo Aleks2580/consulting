b51a68acd33d0ea2ce44fffc474a1476a8e3750a
Aleksei Vlasov <alekseivlasov@Alekseis-Mac-mini.local>
2024-04-30 23:27:27 +0800

    hero section 0.9

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <header className={styles.main}>
+      <header className={styles.header_main}>
         <img className={styles.image} src="/logo.png"></img>
         <div className={styles.nav}>
           <span className={styles.link}>About</span>
@@ -14,7 +14,16 @@ export default function Home() {
         </div>
       </header>
       <div className={styles.hero_main}>
-        
+        <div className={styles.hero_content}>
+       
+        <h1 className={styles.hero_title}>Strategic Consulting and Research Solutions <span className={styles.span}> Globally</span></h1>
+        <p className={styles.hero_text}>
+          At OrientPath, we are committed to creating significant impact and
+          delivering measurable results worldwide. Since 2016, we've been at the
+          forefront of providing expert strategies and insights, ensuring
+          success for diverse markets across the globe.
+        </p>
+        </div>
       </div>
     </main>
   );
