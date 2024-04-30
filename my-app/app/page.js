a48bc1cdc87a90c82aaa32137f78068782ea4517
Aleksei Vlasov <alekseivlasov@Alekseis-Mac-mini.local>
2024-05-01 00:16:28 +0800

    scroll indicator added

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -7,7 +6,7 @@ export default function Home() {
       <header className={styles.header_main}>
         <img className={styles.image} src="/logo.png"></img>
         <div className={styles.nav}>
-          <span className={styles.link}>About</span>
+          <span className={styles.link}>About Us</span>
           <span className={styles.link}>Services</span>
           <span className={styles.link}>Cases</span>
           <span className={styles.link}>Contacts</span>
@@ -15,14 +14,21 @@ export default function Home() {
       </header>
       <div className={styles.hero_main}>
         <div className={styles.hero_content}>
-       
-        <h1 className={styles.hero_title}>Strategic Consulting and Research Solutions <span className={styles.span}> Globally</span></h1>
-        <p className={styles.hero_text}>
-          At OrientPath, we are committed to creating significant impact and
-          delivering measurable results worldwide. Since 2016, we've been at the
-          forefront of providing expert strategies and insights, ensuring
-          success for diverse markets across the globe.
-        </p>
+          <h1 className={styles.hero_title}>
+            Strategic Consulting and Research Solutions{" "}
+            <span className={styles.span}> Globally</span>
+          </h1>
+          <p className={styles.hero_text}>
+            At OrientPath, we are committed to creating significant impact and
+            delivering measurable results worldwide. Since 2016, we've been at
+            the forefront of providing expert strategies and insights, ensuring
+            success for diverse markets across the globe.
+          </p>
+        </div>
+      </div>
+      <div className={styles.mouse}>
+        <div className={styles.mouse_icon}>
+          <span className={styles.mouse_wheel}></span>
         </div>
       </div>
     </main>
