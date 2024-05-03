6f24b36c216c5cf13322d7b026eaea96debe7a05
Aleksei Vlasov <alekseivlasov@Alekseis-Mac-mini.local>
2024-05-03 11:06:24 +0800

    cases done

--- my-app/app/ui/cases/cases.js
+++ my-app/app/ui/cases/cases.js
@@ -1,36 +1,51 @@
-import styles from "./cases.module.css"
-
-
-const casesData = [
-  { id: 1, name: 'Case 1', description: 'Description for Case 1' },
-  { id: 2, name: 'Case 2', description: 'Description for Case 2' },
-  { id: 3, name: 'Case 3', description: 'Description for Case 3' },
-  { id: 4, name: 'Case 4', description: 'Description for Case 3' },
-  { id: 5, name: 'Case 5', description: 'Description for Case 3' },
-  { id: 6, name: 'Case 3', description: 'Description for Case 3' },
-  { id: 7, name: 'Case 3', description: 'Description for Case 3' },
-  { id: 8, name: 'Case 3', description: 'Description for Case 3' },
-];
+import styles from "./cases.module.css";
+import Image from "next/image";
 
 export default function Cases() {
   return (
     <section className={styles.cases_section}>
-    <h2 className={styles.section_title}>Cases</h2>
-    <div className={styles.cases_grid}>
-      {casesData.map((caseItem) => (
-        <div key={caseItem.id} className={styles.flip_card}>
-          <div className={styles.flip_card_inner}>
-            <div className={styles.flip_card_front}>
-              <h3>{caseItem.name}</h3>
-            </div>
-            <div className={styles.flip_card_back}>
-              <p>{caseItem.description}</p>
-            </div>
+      <div className={styles.header}>
+        <h1 className={styles.header_title}>Successful Partnerships</h1>
+        <p className={styles.header_description}>Discover how our strategic insights and innovative approaches have driven success and satisfaction for our clients across various industries.</p>
+      </div>
+      <div className={styles.case_container}>
+        <div className={styles.case_item}>
+          <div className={styles.image_container}>
+            <Image src="/biogenhealth.webp" alt="BioGenHealth Case" width={300} height={300} className={styles.case_image} />
+          </div>
+          <div className={styles.case_content}>
+            <h2 className={styles.case_title}>BioGenHealth</h2>
+            <p className={styles.case_description}>Streamlined operational processes and implemented digital tools, increasing production efficiency by 40%.</p>
           </div>
         </div>
-      ))}
-    </div>
-  </section>
-    
-  )
+        <div className={styles.case_item}>
+          <div className={styles.image_container}>
+            <Image src="/finsecure.webp" alt="FinSecure Case" width={300} height={300} className={styles.case_image} />
+          </div>
+          <div className={styles.case_content}>
+            <h2 className={styles.case_title}>FinSecure</h2>
+            <p className={styles.case_description}>Developed a comprehensive risk management framework, reducing operational risks by 30%.</p>
+          </div>
+        </div>
+        <div className={styles.case_item}>
+          <div className={styles.image_container}>
+            <Image src="/ecosynthetics.webp" alt="EcoSynthetics Case" width={300} height={300} className={styles.case_image} />
+          </div>
+          <div className={styles.case_content}>
+            <h2 className={styles.case_title}>EcoSynthetics</h2>
+            <p className={styles.case_description}>Guided through digital transformation, integrating sustainable practices, increasing market share by 25%.</p>
+          </div>
+        </div>
+        <div className={styles.case_item}>
+          <div className={styles.image_container}>
+            <Image src="/medicaresolutions.webp" alt="MediCare Solutions Case" width={300} height={300} className={styles.case_image} />
+          </div>
+          <div className={styles.case_content}>
+            <h2 className={styles.case_title}>MediCare Solutions</h2>
+            <p className={styles.case_description}>Strategic overhaul focusing on regulatory compliance and market analysis, leading position in emerging markets.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
